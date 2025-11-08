ec035ddb512e36071560baa47aff6ccfff8dc24b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-08 15:31:07 -0500

    It works

--- test-broccoli-app/ember-cli-build.js
+++ test-broccoli-app/ember-cli-build.js
@@ -5,6 +5,41 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    // transforms your own app
+    babel: {
+      plugins: [
+        [
+          require.resolve(
+            'babel-plugin-ember-polyfill-get-and-set-owner-from-ember-owner',
+          ),
+          {},
+        ],
+      ],
+    },
+    autoImport: {
+      webpack: {
+        module: {
+          rules: [
+            // transforms v2 addon code
+            {
+              test: (filename) => {
+                return filename.endsWith('.js');
+              },
+              use: {
+                loader: 'babel-loader-8',
+                options: {
+                  plugins: [
+                    require.resolve(
+                      'babel-plugin-ember-polyfill-get-and-set-owner-from-ember-owner',
+                    ),
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
